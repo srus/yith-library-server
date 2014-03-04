06a6796f53d986f7d5bc3e871e673bb030463f9d
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2014-03-03 20:21:21 -0800

    Clean the users collections in the base views tests. Second attempt to fix the intermitent test failure

--- yithlibraryserver/tests/test_views.py
+++ yithlibraryserver/tests/test_views.py
@@ -25,6 +25,8 @@ from yithlibraryserver import testing
 
 class ViewTests(testing.TestCase):
 
+    clean_collections = ('users', )
+
     def test_home(self):
         res = self.testapp.get('/')
         self.assertEqual(res.status, '200 OK')
