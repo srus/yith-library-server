17dbc4b8c6acf755d98e538071201e55ac060317
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2015-03-19 21:09:28 -0700

    Update copyright years

--- yithlibraryserver/contributions/__init__.py
+++ yithlibraryserver/contributions/__init__.py
@@ -1,6 +1,5 @@
-
 # Yith Library Server is a password storage server.
-# Copyright (C) 2013 Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
+# Copyright (C) 2013-2015 Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
 #
 # This file is part of Yith Library Server.
 #
