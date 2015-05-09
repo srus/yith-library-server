f93a801a7951f2ee1a59b536608e13928aa60102
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2015-05-08 19:58:49 -0700

    Add simple test for the createdb script

---
+++ yithlibraryserver/scripts/tests/test_createdb.py
@@ -0,0 +1,60 @@
+# Yith Library Server is a password storage server.
+# Copyright (C) 2015 Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
+#
+# This file is part of Yith Library Server.
+#
+# Yith Library Server is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Yith Library Server is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Yith Library Server.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+
+from yithlibraryserver.compat import StringIO
+from yithlibraryserver.scripts.createdb import createdb
+from yithlibraryserver.scripts.testing import ScriptTests
+
+
+class BuildAssetsTests(ScriptTests):
+
+    use_db = False
+
+    def setUp(self):
+        super(BuildAssetsTests, self).setUp()
+
+        # Save sys values
+        self.old_args = sys.argv[:]
+        self.old_stdout = sys.stdout
+
+    def tearDown(self):
+        # Restore sys.values
+        sys.argv = self.old_args
+        sys.stdout = self.old_stdout
+
+        super(BuildAssetsTests, self).tearDown()
+
+    def test_no_arguments(self):
+        # Replace sys argv and stdout
+        sys.argv = []
+        sys.stdout = StringIO()
+
+        result = createdb()
+        self.assertEqual(result, 2)
+        stdout = sys.stdout.getvalue()
+        self.assertEqual(stdout, 'You must provide at least one argument\n')
+
+    def test_normal_usage(self):
+        sys.argv = ['notused', self.conf_file_path]
+        sys.stdout = StringIO()
+        result = createdb()
+        self.assertEqual(result, None)
+        stdout = sys.stdout.getvalue()
+        self.assertEqual(stdout, '')
